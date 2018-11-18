9120545cc726461e7af618024bce30418d9a5728
scritian <scritian@yahoo.com>
2018-11-18 18:26:15 +0200

    Trip Manager app

---
+++ googlecourse/app/src/main/java/com/scritian/googlecourse/LoginActivity.java
@@ -0,0 +1,46 @@
+package com.scritian.googlecourse;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private Button btnLogin;
+    private EditText etUserName;
+    private EditText etPassword;
+    private String registeredUser= "test";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        btnLogin=findViewById(R.id.bLogin);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String username= etUserName.getText().toString();
+                String password=etPassword.getText().toString();
+                if(password.equals(registeredUser) & username.equals(registeredUser)) {
+                    Toast.makeText(LoginActivity.this, "You have succesfully logged in!", Toast.LENGTH_SHORT).show();
+                    //setContentView(R.layout.activity_main);
+                    startActivity(new Intent(LoginActivity.this, MainActivity.class));
+                }
+                else if(!password.equals(registeredUser) & username.equals(registeredUser))
+                    Toast.makeText(LoginActivity.this, "Wrong password!", Toast.LENGTH_SHORT).show();
+                else if(password.equals(registeredUser) & !username.equals(registeredUser))
+                    Toast.makeText(LoginActivity.this, "Wrong username!", Toast.LENGTH_SHORT).show();
+                else
+                    Toast.makeText(LoginActivity.this, "Wrong username and password!", Toast.LENGTH_SHORT).show();
+                //Log.d(  "Login activity",  "Login Pressed");
+            }
+        });
+        etUserName=findViewById(R.id.etUsername);
+        etPassword=findViewById(R.id.etPassword);
+    }
+}
--- googlecourse/app/src/main/java/com/scritian/googlecourse/MainActivity.java
+++ googlecourse/app/src/main/java/com/scritian/googlecourse/MainActivity.java
@@ -2,13 +2,68 @@ package com.scritian.googlecourse;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.RatingBar;
+import android.widget.SeekBar;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    @Override
+    private Button btnSave;
+    private EditText etTripName;
+    private EditText etDestination;
+    private RadioGroup rgTripType;
+    private RadioButton rbSelected;
+    private SeekBar sbPrice;
+    private RatingBar rbRating;
+    private EditText etStartDate, etEndDate;
+    private TextView tvPrice;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
+        rgTripType=findViewById(R.id.rgTripType);
+        btnSave=findViewById(R.id.bSave);
+        tvPrice=findViewById(R.id.tvPrice);
+        sbPrice=findViewById(R.id.sbPrice);
+        sbPrice.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                tvPrice.setText("Price("+progress+"€)");
+            }
 
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+        btnSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int selected_id=rgTripType.getCheckedRadioButtonId();
+                rbSelected=findViewById(selected_id);
+                etTripName=findViewById(R.id.etTripName);
+                etDestination=findViewById(R.id.etDestination);
+                sbPrice=findViewById(R.id.sbPrice);
+                rbRating=findViewById(R.id.rbRating);
+                etStartDate=findViewById(R.id.etStartDate);
+                etEndDate=findViewById(R.id.etEndDate);
+                String result="";
+                result+="Destination: "+etTripName.getText().toString()+"\nDestination: "+etDestination.getText().toString();
+                result+="\nTrip type: "+rbSelected.getText().toString()+"\nPrice(€): "+sbPrice.getProgress();
+                result+="€\nStart date: "+etStartDate.getText().toString()+"\nEnd date: "+etEndDate.getText().toString();
+                result+="\nRating: "+rbRating.getRating()+" stars";
+                Toast.makeText(MainActivity.this, result, Toast.LENGTH_LONG).show();
+            }
+        });
+    }
 }
